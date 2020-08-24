82bf6515d6504f7ee3d7b13f8dad6e2a9fd61c30
yali <yali860208@gmail.com>
2020-08-24 16:08:08 +0800

    tttttttt

--- app.py
+++ app.py
@@ -224,6 +224,6 @@ if __name__ == "__main__":
     # app.run(port=5000)
     # 連到heroku
     import os
-    port = int(os.environ.egt('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
     
