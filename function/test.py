4906a491862a2279bee76fe3f4085a2536919daf
yali <yali860208@gmail.com>
2020-08-28 11:06:46 +0800

    k

--- function/test.py
+++ function/test.py
@@ -1,23 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import gspread
+import random
+from oauth2client.service_account import ServiceAccountCredentials
 
-url = 'https://www.dcard.tw/f'
-webContent = requests.get(url)
-webContent.encoding = 'UTF-8'
-soup = BeautifulSoup(webContent.text, 'html.parser')
+scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('HP2020-6545eb07aff6.json',scope)
+client = gspread.authorize(creds)
+spreadSheet = client.open('hp2020linebot')
 
-for i in soup.select('article'):
-    title = i.select('h2 a span')[0].text
-    article = i.select('div div span')[4].text
-    # if article == '回應':
-    #     article = i.select('div div')[3].text
+workSheet_status = spreadSheet.worksheet('status')
+workSheet_worldcupQ = spreadSheet.worksheet('worldcupQ')
+workSheet_worldcupA = spreadSheet.worksheet('worldcupA')
 
-    if article == title:
-        article = i.select('div div span')[5].text
-    if article == '回應':
-        article = i.select('div div')[4].text
-    if article.isdigit() == True:
-        article = i.select('div div span')[2].text
-    print(article)
-    print('-------------------')
+nameList = [1,2,3,4,5]
+workSheet_worldcupA.delete_row(1)
+workSheet_worldcupA.insert_row(nameList,index=5)
+nameList = workSheet_worldcupA.row_values(userSend)
+
+
+# url = 'https://www.dcard.tw/f'
+# webContent = requests.get(url)
+# webContent.encoding = 'UTF-8'
+# soup = BeautifulSoup(webContent.text, 'html.parser')
+
+# for i in soup.select('article'):
+#     title = i.select('h2 a span')[0].text
+#     article = i.select('div div span')[4].text
+#     # if article == '回應':
+#     #     article = i.select('div div')[3].text
+
+#     if article == title:
+#         article = i.select('div div span')[5].text
+#     if article == '回應':
+#         article = i.select('div div span')[3].text
+#     if article.isdigit() == True:
+#         article = i.select('div div span')[2].text
+#     print(article)
+#     print('-------------------')
